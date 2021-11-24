ace5b38e529a395359fad395ef637c76c4ba307e
Natalia Lopez <natalia.lopez17@upr.edu>
2021-11-24 02:17:57 -0400

    it should work now, with 40 minutes. gotta try fetching all the links at once.

--- Yearly_Solar_Data.py
+++ Yearly_Solar_Data.py
@@ -5,6 +5,7 @@ Created on October 8, 2021
 @author Natalia López
 """
 import json
+import time
 from datetime import datetime
 import requests
 import zipfile
@@ -39,31 +40,27 @@ Returns:
 response: The download link that contains the file.
 '''
 
-#url_nw, url_ne, url_sw, url_se, url_c, url_v
-def TimeoutHTTPAdapter(url_v):
+def TimeoutHTTPAdapter(url_nw, url_ne, url_sw, url_se, url_c, url_v):
     retry_strategy = Retry(
         total=3,
         status_forcelist=[404, 400, 405, 403, 429, 500, 502, 503, 504],
         allowed_methods=["HEAD", "GET", "OPTIONS", "POST"],
-        backoff_factor=10
+        backoff_factor=1
     )
     adapter = HTTPAdapter(max_retries=retry_strategy)
     http = requests.Session()
     http.mount("https://", adapter)
     print("passed http.mount", "\n")
-    #response_nw = http.get(url_nw, timeout=10)
-    # response_ne = http.get(url_ne, timeout=10)
-    # print("response passed", "\n")
-    # response_sw = http.get(url_sw, timeout=10)
-    # print("response sw passed")
-    # response_se = http.get(url_se, timeout=10)
-    # print("response se", response_se, "\n")
-    # response_c = http.get(url_c, timeout=10)
-    # print("response c", response_c, "\n")
+    response_nw = http.get(url_nw, timeout=10)
+    response_ne = http.get(url_ne, timeout=10)
+    print("fetching response_sw...")
+    response_sw = http.get(url_sw, timeout=10)
+    response_se = http.get(url_se, timeout=10)
+    response_c = http.get(url_c, timeout=10)
     response_v = http.get(url_v, timeout=10)
-    print("response v", response_v, "\n")
-    # return response_nw  response_ne, response_sw, response_se, response_c, response_v
-    return response_v
+    print("response fetched", response_sw, "\n")
+    return response_nw, response_ne, response_sw, response_se, response_c, response_v
+    # return response_sw
 
 
 '''
@@ -183,29 +180,29 @@ download_url: This will be the url utilized to download the file generated with 
 ___________________________________________________________________
 '''
 
-#download_url_ne, download_url_sw, download_url_se,download_url_c, download_url_v,
-def download_zip(download_url_v, dest={destination}):
-    # solar_res_nw = requests.get(download_url_nw)
-    #solar_res_ne = requests.get(download_url_ne)
-    #solar_res_sw = requests.get(download_url_sw)
-    # solar_res_se = requests.get(download_url_se)
-    # solar_res_c = requests.get(download_url_c)
+#,
+def download_zip(download_url_nw,download_url_ne, download_url_sw, download_url_se,download_url_c, download_url_v, dest={destination}):
+    solar_res_nw = requests.get(download_url_nw)
+    solar_res_ne = requests.get(download_url_ne)
+    solar_res_sw = requests.get(download_url_sw)
+    solar_res_se = requests.get(download_url_se)
+    solar_res_c = requests.get(download_url_c)
     solar_res_v = requests.get(download_url_v)
 
 
-    # zip_file_nw = zipfile.ZipFile(io.BytesIO(solar_res_nw.content))
-    # zip_file_ne = zipfile.ZipFile(io.BytesIO(solar_res_ne.content))
-    #zip_file_sw = zipfile.ZipFile(io.BytesIO(solar_res_sw.content))
-    # zip_file_se = zipfile.ZipFile(io.BytesIO(solar_res_se.content))
-    # zip_file_c = zipfile.ZipFile(io.BytesIO(solar_res_c.content))
+    zip_file_nw = zipfile.ZipFile(io.BytesIO(solar_res_nw.content))
+    zip_file_ne = zipfile.ZipFile(io.BytesIO(solar_res_ne.content))
+    zip_file_sw = zipfile.ZipFile(io.BytesIO(solar_res_sw.content))
+    zip_file_se = zipfile.ZipFile(io.BytesIO(solar_res_se.content))
+    zip_file_c = zipfile.ZipFile(io.BytesIO(solar_res_c.content))
     zip_file_v = zipfile.ZipFile(io.BytesIO(solar_res_v.content))
 
-
-    # zip_file_nw.extractall(dest)
-    # zip_file_ne.extractall(dest)
-    #zip_file_sw.extractall(dest)
-    # zip_file_se.extractall(dest)
-    # zip_file_c.extractall(dest)
+    print("downloading zip file to destination....")
+    zip_file_nw.extractall(dest)
+    zip_file_ne.extractall(dest)
+    zip_file_sw.extractall(dest)
+    zip_file_se.extractall(dest)
+    zip_file_c.extractall(dest)
     zip_file_v.extractall(dest)
 
 
@@ -218,48 +215,33 @@ if __name__ == "__main__":
     items = range(10)
     if start_year.isdigit() and end_year.isdigit() and interval.isdigit():
         if int(start_year) >= 2018:
-            # response_nw = nw_request_builder(int(start_year), int(end_year), int(interval), email)
-            # response_ne = ne_request_builder(int(start_year), int(end_year), int(interval), email)
-            # print("response_ne", response_ne, "\n")
-            # response_sw = sw_request_builder(int(start_year), int(end_year), int(interval), email)
-            # print("response sw", response_sw, "\n")
-            # response_se = se_request_builder(int(start_year), int(end_year), int(interval), email)
-            # print("response se", response_se, "\n")
-            # response_c = c_request_builder(int(start_year), int(end_year), int(interval), email)
-            # print("response c", response_c, "\n")
+            response_nw = nw_request_builder(int(start_year), int(end_year), int(interval), email)
+            response_ne = ne_request_builder(int(start_year), int(end_year), int(interval), email)
+            response_sw = sw_request_builder(int(start_year), int(end_year), int(interval), email)
+            response_se = se_request_builder(int(start_year), int(end_year), int(interval), email)
+            response_c = c_request_builder(int(start_year), int(end_year), int(interval), email)
             response_v = v_request_builder(int(start_year), int(end_year), int(interval), email)
-            print("response v", response_v, "\n")
-
-            if response_v.status_code == 200: #and response_ne.status_code == 200 \
-                    #and response_sw.status_code == 200 and response_se.status_code == 200 \
-                    #and response_c.status_code == 200 and response_v.status_code == 200:
-                # jsonResponse_nw = response_nw.json()
-                # jsonResponse_ne = response_ne.json()
-                # jsonResponse_sw = response_sw.json()
-                # jsonResponse_se = response_se.json()
-                # jsonResponse_c = response_c.json()
+            
+            if response_sw.status_code == 200 and response_ne.status_code == 200 \
+                    and response_sw.status_code == 200 and response_se.status_code == 200 \
+                    and response_c.status_code == 200 and response_v.status_code == 200:
+                jsonResponse_nw = response_nw.json()
+                jsonResponse_ne = response_ne.json()
+                jsonResponse_sw = response_sw.json()
+                jsonResponse_se = response_se.json()
+                jsonResponse_c = response_c.json()
                 jsonResponse_v = response_v.json()
-                # print("json response nw", jsonResponse_nw, "\n")
-                # print("json response ne", jsonResponse_ne, "\n")
-                # print("json response sw", jsonResponse_sw, "\n")
-                # print("json response se", jsonResponse_se, "\n")
-                # print("json response c", jsonResponse_c, "\n")
-                print("json response v", jsonResponse_v, "\n")
-
-                # download_url_nw = jsonResponse_nw['outputs']['downloadUrl']
-                # download_url_ne = jsonResponse_ne['outputs']['downloadUrl']
-                # download_url_sw = jsonResponse_sw['outputs']['downloadUrl']
-                # download_url_se = jsonResponse_se['outputs']['downloadUrl']
-                # download_url_c = jsonResponse_c['outputs']['downloadUrl']
+                
+                download_url_nw = jsonResponse_nw['outputs']['downloadUrl']
+                download_url_ne = jsonResponse_ne['outputs']['downloadUrl']
+                download_url_sw = jsonResponse_sw['outputs']['downloadUrl']
+                download_url_se = jsonResponse_se['outputs']['downloadUrl']
+                download_url_c = jsonResponse_c['outputs']['downloadUrl']
                 download_url_v = jsonResponse_v['outputs']['downloadUrl']
-                # print("download_url nw", download_url_nw, "\n")
-                # print("download_url ne", download_url_ne, "\n")
-                # print("download_url sw", download_url_sw, "\n")
-                # print("download_url se", download_url_se, "\n")
-                # print("download_url c", download_url_c, "\n")
-                print("download_url v", download_url_v, "\n")
-
-                TimeoutHTTPAdapter(download_url_v)
-                download_zip(download_url_v,destination)
+                
+                print("waiting 40 minutes....zzz....")
+                time.sleep(2400)
+                TimeoutHTTPAdapter(download_url_sw) 
+                download_zip(download_url_sw,destination)
         else:
             print("Enter a valid year starting at 2018 after the script name.")
